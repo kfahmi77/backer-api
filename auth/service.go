1cfd6e46c1f4d5d4f5a21a781a0599a440ccaec5
kfahmi77 <khoirulfahmi44@gmail.com>
2023-11-26 16:47:17 +0700

    generate jwt token

---
+++ auth/service.go
@@ -0,0 +1,24 @@
+package auth
+
+import "github.com/golang-jwt/jwt"
+
+type Service interface {
+	GenerateToken(userID int) (string, error)
+}
+
+type jwtService struct {
+}
+
+var SecretKey = []byte("BWABACKER_s3cret")
+
+func (s *jwtService) GenerateToken(userID int) (string, error) {
+	claim := jwt.MapClaims{}
+	claim["user_id"] = userID
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claim)
+	signedToken, err := token.SignedString(SecretKey)
+	if err != nil {
+		return signedToken, err
+	}
+	return signedToken, nil
+}
