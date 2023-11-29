161ea9756112d67e5102feef0927a21fc2969ae7
kfahmi77 <khoirulfahmi44@gmail.com>
2023-11-29 11:24:03 +0700

    create get list campaign

--- campaign/repository.go
+++ campaign/repository.go
@@ -1,1 +1,35 @@
 package campaign
+
+import "gorm.io/gorm"
+
+type Repository interface {
+	FindAll() ([]Campaign, error)
+	FindUserID(userID int) ([]Campaign, error)
+}
+
+type repository struct {
+	db *gorm.DB
+}
+
+func NewRepository(db *gorm.DB) *repository {
+	return &repository{db}
+}
+
+func (r *repository) FindAll() ([]Campaign, error) {
+	var campaigns []Campaign
+
+	err := r.db.Find(campaigns).Error
+	if err != nil {
+		return campaigns, err
+	}
+	return campaigns, nil
+}
+
+func (r *repository) FindUserId(userId int) ([]Campaign, error) {
+	var campaigns []Campaign
+	err := r.db.Where("user_id = ?", userId).Find(&campaigns).Error
+	if err != nil {
+		return campaigns, err
+	}
+	return campaigns, nil
+}
